7e365c6add31dadc5436f2ea82c54f4fec9690b5
AnBinh1703 <dan1314705@gmail.com>
2024-01-10 17:04:39 +0700

    create demo project

---
+++ src/main/java/com/example/be_interiorconstructionquotationsystem/BeInteriorConstructionQuotationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_interiorconstructionquotationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeInteriorConstructionQuotationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeInteriorConstructionQuotationSystemApplication.class, args);
+	}
+
+}
